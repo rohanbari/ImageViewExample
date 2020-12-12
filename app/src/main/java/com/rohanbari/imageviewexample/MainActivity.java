956c41ec0ef5006d1ac0e792fc50c67b94db273a
rohanbari <rohanbari4@gmail.com>
2020-12-12 23:24:37 +0530

    Glide library used for loading random images

--- app/src/main/java/com/rohanbari/imageviewexample/MainActivity.java
+++ app/src/main/java/com/rohanbari/imageviewexample/MainActivity.java
@@ -6,17 +6,26 @@ import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
+    private final int[] images = new int[]{R.drawable.greece, R.drawable.san_francisco, R.drawable.indiana};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.image);
     }
 
     public void shuffleImage(View view) {
-        ImageView imageView = findViewById(R.id.image);
-        imageView.setImageResource(R.mipmap.san_francisco);
+        int id = images[new Random().nextInt(images.length)];
+        Glide.with(this).load(id).into(imageView);
+
         imageView.setCropToPadding(true);
     }
 }
